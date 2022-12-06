aa27e1e1e72020deb52f1d64b70e9daeaa2cca1a
mokronos <sebastian@hirts.de>
2022-12-06 07:05:41 +0100

    day 6 done

---
+++ 2022/6_2.py
@@ -0,0 +1,19 @@
+data = open("data6.txt", "r").read()
+
+# data = "bvwbjplbgvbhsrlpgdmjqwftvncz"
+def solve(data):
+
+    print(data)
+    n = 14
+    for i, letter in enumerate(data):
+
+        if i < n:
+            continue
+
+        if len(set(data[i-n:i])) == n:
+            print(data[i-n:i])
+            print(i)
+            break
+
+
+solve(data)
