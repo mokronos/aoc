252f47e4faf988f56d45c858ad59fd80c2056ea3
mokronos <sebastian@hirts.de>
2022-12-25 03:56:14 +0100

    day 21 done, pretty efficiently, but doesn't generalize well

--- 2022/21_2.py
+++ 2022/21_2.py
@@ -6,93 +6,94 @@ def solve(data):
 
     seen = dict()
     q = dict()
-    root1 = None
-    root2 = None
-    global c
-    c = 0
+    ops = {"+": lambda x, y: x + y, "-": lambda x, y: x - y, "*": lambda x, y: x * y, "/": lambda x, y: x / y}
     for line in data.splitlines():
         r = r"(.*): (.*) ([\+\-\/\*]) (.*)"
         if x := re.findall(r, line):
             x = x[0]
-            if x[0] == "root":
-                root1 = x[1]
-                root2 = x[3]
-                continue
+            # if x[0] == "root":
+            #     continue
             q[x[0]] = (x[1], x[2], x[3])
         else:
             x = line.split(": ")
             seen[x[0]] = int(x[1])
 
-
-    def search(node):
+    def search(seen, node, found = 0):
         if node == "humn":
-            global c
-            c = 1
+            found = 1
         if node in seen:
-            return seen[node]
+            return seen[node], found, seen
         else:
             x = q[node]
-            if x[1] == "+":
-                seen[node] = search(x[0]) + search(x[2])
-            elif x[1] == "-":
-                seen[node] = search(x[0]) - search(x[2])
-            elif x[1] == "*":
-                seen[node] = search(x[0]) * search(x[2])
-            elif x[1] == "/":
-                seen[node] = search(x[0]) / search(x[2])
-            return seen[node]
+            x0, found, seen = search(seen, x[0], found)
+            x2, found, seen = search(seen, x[2], found)
+            func = ops[x[1]]
+            seen[node] = func(x0, x2)
+
+            return seen[node], found, seen
+
+    def back(seen, start, end = "humn"):
+
+        if start == end:
+            return seen[start]
+
+        x = q[start]
+        node1 = x[0]
+        node2 = x[2]
+        
+        lv, lc,_ = search(seen.copy(), node1)
+        rv, rc,_ = search(seen.copy(), node2)
+
+        sval = seen[start]
+        val = rv if lc else lv
+        new_start = node1 if lc else node2
+
+        if x[1] == "+":
+            seen[new_start] = sval - val
+
+        elif x[1] == "*":
+            seen[new_start] = sval / val
+
+        elif x[1] == "-":
+            if lc:
+                # left side is x
+                # sval = x - val
+                seen[new_start] = sval + val
+            else:
+                # right side is x
+                # sval = val - x
+                seen[new_start] = val - sval
+
+        elif x[1] == "/":
+            if lc:
+                # left side is x
+                # sval = x / val
+                seen[new_start] = sval * val
+            else:
+                # right side is x
+                # sval = val / x
+                seen[new_start] = val / sval
+
+        return back(seen, new_start)
+
+
+    node1 = q["root"][0]
+    node2 = q["root"][2]
     
-    def get_path(node):
-        if node == "humn":
-            return "humn"
-        if node in seen:
-            return str(seen[node])
-        else:
-            x = q[node]
+    # check which side of the root the humn node is in?
+    lv, lc,_ = search(seen.copy(), node1)
+    rv, rc,_ = search(seen.copy(), node2)
 
-            path = get_path(x[0]) + " " + x[1] + " " + get_path(x[2])
+    # calculate the side without the humn node
+    val = rv if lc else lv
+    start = node1 if lc else node2
 
+    print(f"need to get val for humn, so that {start} = {val}")
 
-    # check if one of the variables of root can be calculated
-    org_seen = seen.copy()
-    org_q = q.copy()
+    # search backward from other node, check at every fork where humn is, calculate other side to calculate needed value
 
-    search(root1)
-    if c:
-        to_search = root1
-    else:
-        to_search = root2
-
-    seen = org_seen.copy()
-    q = org_q.copy()
-
-    target = search(root1 if root2 == to_search else root2)
-
-    print("target", target)
-    print("to_search", to_search)
-
-    
-    # search backwards for the value of to_search
-    seen = org_seen.copy()
-    q = org_q.copy()
-
-    def backwards(node, target):
-
-        if node == "humn":
-            return
-
-        if node in seen:
-            return seen[node]
-        else:
-            x = q[node]
-            if x[1] == "+":
-                
-            elif x[1] == "-":
-                seen[node] = backwards(x[0], target) - backwards(x[2], target)
-            elif x[1] == "*":
-                seen[node] = backwards(x[0], target) * backwards(x[2], target)
-            elif x[1] == "/":
-                seen[node] = backwards(x[0], target) / backwards(x[2], target)
-            return seen[node]
+    seen[start] = val
+    result = back(seen.copy(), start)
+    print(f"result: {int(result)}")
 
 solve(data)
