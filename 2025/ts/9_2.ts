4d64f647bbe0b04c97f7128d5c2d0c89074c892b
Sebastian Hirt <sebastian@hirts.de>
2025-12-09 13:15:15 +0100

    feat: day 9 part 1 done

---
+++ 2025/ts/9_2.ts
@@ -0,0 +1,34 @@
+import { readFileSync } from "fs";
+
+const data_path = './data9.txt'
+
+const raw_data = readFileSync(data_path).toString().trim()
+
+// const raw_data = `7,1
+// 11,1
+// 11,7
+// 9,7
+// 9,5
+// 2,5
+// 2,3
+// 7,3`.trim()
+
+const data: string[] = raw_data.split('\n')
+
+var max_area = 0
+
+for (let i = 0; i < data.length - 1; i++) {
+    for (let j = i + 1; j < data.length; j++) {
+
+        const line_a = data[i]
+        const line_b = data[j]
+
+        const [x_a, y_a] = line_a.split(',').map(Number)
+        const [x_b, y_b] = line_b.split(',').map(Number)
+
+        const area = (Math.abs(x_a - x_b) + 1) * (Math.abs(y_a - y_b) + 1)
+        max_area = Math.max(max_area, area)
+    }
+}
+
+console.log(max_area)
